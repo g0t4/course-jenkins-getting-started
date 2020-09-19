19af8f4ac1b385cff051d7cab525a87b8a2fc74a
Wes Higbee <wes.mcclure@gmail.com>
2020-09-19 06:34:32 -0400

    initial email that is sent on every build

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -9,13 +9,22 @@ pipeline {
         }
         stage('Build') {
             steps {
-                sh './mvnw clean package'
+                //sh './mvnw clean package'
+                sh 'false' // true
             }
         
             post {
                 always {
-                    junit '**/target/surefire-reports/TEST-*.xml'
-                    archiveArtifacts 'target/*.jar'
+                    //junit '**/target/surefire-reports/TEST-*.xml'
+                    //archiveArtifacts 'target/*.jar'
+                //}
+                //changed {
+                    emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
+                        body: "Please go to ${BUILD_URL} and verify the build", 
+                        attachLog: true, 
+                        compressLog: true, 
+                        to: "test@jenkins",
+                        recipientProviders: [upstreamDevelopers(), requestor()]
                 }
             }
         }
